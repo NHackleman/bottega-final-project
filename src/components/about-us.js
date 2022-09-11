74641091e0c8d3473d265c04ccb87149573c8a7b
Nathan <hacklemannathan@gmail.com>
2022-09-10 21:06:17 -0500

    Added background image and body paragraph/header to about-us page

--- src/components/about-us.js
+++ src/components/about-us.js
@@ -1,26 +1,37 @@
 import React from "react";
 
+const aboutUsStyles = {
+	backgroundImage: "url(/assets/controller.jpg)",
+	backgroundSize: "cover",
+};
+
 function aboutUs() {
 	return (
-		<div>
-			<div>About Us Page</div>
-			<div>
-				Esse adipisicing amet dolor sunt culpa aute ea amet est
-				excepteur. Ea reprehenderit aliquip ipsum id nostrud incididunt
-				proident cupidatat tempor sunt ut. Laborum eiusmod duis
-				reprehenderit sunt ipsum eiusmod ad eiusmod culpa. Adipisicing
-				nostrud excepteur tempor ipsum ullamco non do voluptate anim.
-				Sit est adipisicing excepteur id dolor nostrud ea ea est amet eu
-				velit qui. Labore dolore ut dolor incididunt. Sunt ipsum fugiat
-				dolore officia quis sint esse voluptate nulla consequat. Commodo
-				do do sint ullamco id labore nulla culpa cupidatat sint magna
-				duis adipisicing nostrud. Cupidatat duis occaecat labore
-				excepteur do. Veniam nulla adipisicing est anim esse nostrud id
-				laborum aute do do Lorem. Dolor aute dolor sint qui anim culpa
-				Lorem do voluptate et minim et laboris. Laborum ipsum non
-				reprehenderit irure mollit. Laborum culpa eu nulla excepteur
-				consequat ipsum dolor adipisicing elit labore dolore. Veniam non
-				sunt in non labore nulla eu nisi ullamco pariatur.
+		<div className="about-us-container" style={aboutUsStyles}>
+			<div className="about-us-body">
+				<div className="about-us-header">About Us</div>
+				<p>
+					Esse adipisicing amet dolor sunt culpa aute ea amet est
+					excepteur. Ea reprehenderit aliquip ipsum id nostrud
+					incididunt proident cupidatat tempor sunt ut. Laborum
+					eiusmod duis reprehenderit sunt ipsum eiusmod ad eiusmod
+					culpa. Adipisicing nostrud excepteur tempor ipsum ullamco
+					non do voluptate anim. Sit est adipisicing excepteur id
+					dolor nostrud ea ea est amet eu velit qui. Labore dolore ut
+					dolor incididunt. Sunt ipsum fugiat dolore officia quis sint
+					esse voluptate nulla consequat.
+				</p>
+				<p>
+					Commodo do do sint ullamco id labore nulla culpa cupidatat
+					sint magna duis adipisicing nostrud. Cupidatat duis occaecat
+					labore excepteur do. Veniam nulla adipisicing est anim esse
+					nostrud id laborum aute do do Lorem. Dolor aute dolor sint
+					qui anim culpa Lorem do voluptate et minim et laboris.
+					Laborum ipsum non reprehenderit irure mollit. Laborum culpa
+					eu nulla excepteur consequat ipsum dolor adipisicing elit
+					labore dolore. Veniam non sunt in non labore nulla eu nisi
+					ullamco pariatur.
+				</p>
 			</div>
 		</div>
 	);
