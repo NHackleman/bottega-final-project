bde15f258ba738c066ffc03d0fae7c0e1a461c74
Nathan <hacklemannathan@gmail.com>
2022-10-07 19:11:22 -0500

    Created backend folder with simple API built in Flask. Made simple calls to this API in merch-body component. Updated .gitignore to ignore the cors-anywhere folder

--- src/components/merch-body.js
+++ src/components/merch-body.js
@@ -1,5 +1,45 @@
-import React from "react";
+import React, { useState, useEffect, Component } from "react";
+import axios from "axios";
 
-export default function merchBody() {
-	return <div>Merch body goes here</div>;
+class MerchBody extends Component {
+	constructor() {
+		super();
+
+		// const [data, setdata] = useState({
+		this.state = {
+			name: "",
+			about: "",
+		};
+
+		// });
+		const corsAnywhereUrl = "https://vast-woodland-10845.herokuapp.com/";
+	}
+
+	componentDidMount() {
+		axios
+			.get(`http://localhost:5000/profile`)
+			.then((res) => {
+				this.setState({
+					name: res.data.name,
+					about: res.data.about,
+				});
+				console.log("Response", res);
+			})
+			.catch((err) => {
+				console.error("Fetch error", err);
+			});
+	}
+
+	render() {
+		return (
+			<div>
+				<h1 className="api-data">API Data</h1>
+
+				<h3 className="api-data">{this.state.name}</h3>
+				<h3 className="api-data">{this.state.about}</h3>
+			</div>
+		);
+	}
 }
+
+export default MerchBody;
