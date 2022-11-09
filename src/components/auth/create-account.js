ff18154bfaae8d67cd24e91f07c08db64415774a
Nathan <hacklemannathan@gmail.com>
2022-11-08 20:05:18 -0600

    Changed login and create-account components to forms so password fields are censored. Added redirect to homepage after logging in/creating account

--- src/components/auth/create-account.js
+++ src/components/auth/create-account.js
@@ -8,18 +8,19 @@ import {
 import { NavLink } from "react-router-dom";
 import app from "./firebase";
 
-function createAccount() {
+function createAccount(props) {
 	const [registerEmail, setRegisterEmail] = useState("");
 	const [registerPassword, setRegisterPassword] = useState("");
 
 	const [user, setUser] = useState({});
 	const auth = getAuth(app);
 
-	onAuthStateChanged(auth, (currentUser) => {
-		setUser(currentUser);
-	});
+	// onAuthStateChanged(auth, (currentUser) => {
+	// 	setUser(currentUser);
+	// });
 
-	const register = async () => {
+	const register = async (event) => {
+		event.preventDefault();
 		try {
 			const user = await createUserWithEmailAndPassword(
 				auth,
@@ -30,31 +31,40 @@ function createAccount() {
 		} catch (error) {
 			console.error("Create User error...", error.message);
 		}
+		props.history.push("/");
 	};
 
 	return (
 		<div className="create-account-page">
 			<div className="create-account-wrapper">
 				<h1> Register User </h1>
-				<input
-					placeholder="Email..."
-					onChange={(event) => {
-						setRegisterEmail(event.target.value);
-					}}
-				/>
-				<input
-					placeholder="Password..."
-					onChange={(event) => {
-						setRegisterPassword(event.target.value);
-					}}
-				/>
+				<form onSubmit={register}>
+					<input
+						placeholder="Email..."
+						type="email"
+						onChange={(event) => {
+							setRegisterEmail(event.target.value);
+						}}
+					/>
+					<input
+						type="password"
+						placeholder="Password..."
+						onChange={(event) => {
+							setRegisterPassword(event.target.value);
+						}}
+					/>
 
-				<div className="bottom-buttons-wrapper">
-					<NavLink to="/login" className="login-link">
-						Login
-					</NavLink>
-					<button onClick={register}> Create user </button>
-				</div>
+					<div className="bottom-buttons-wrapper">
+						<NavLink to="/login" className="login-link">
+							Login
+						</NavLink>
+						<input
+							type="submit"
+							value="Register"
+							className="create-account-button"
+						/>
+					</div>
+				</form>
 			</div>
 		</div>
 	);
--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -8,18 +8,32 @@ import {
 import { NavLink } from "react-router-dom";
 import app from "./firebase";
 
-function login() {
+function login(props) {
 	const [loginEmail, setloginEmail] = useState("");
 	const [loginPassword, setloginPassword] = useState("");
 
 	const [user, setUser] = useState({});
 	const auth = getAuth(app);
 
-	onAuthStateChanged(auth, (currentUser) => {
-		setUser(currentUser);
-	});
+	// onAuthStateChanged(auth, (currentUser) => {
+	// 	setUser(currentUser);
+	// });
 
-	const login = async () => {
+	// const login = async () => {
+	// 	try {
+	// 		const user = await signInWithEmailAndPassword(
+	// 			auth,
+	// 			loginEmail,
+	// 			loginPassword
+	// 		);
+	// 		console.log(user);
+	// 	} catch (error) {
+	// 		console.error("Create User error...", error.message);
+	// 	}
+	// };
+
+	const login = async (event) => {
+		event.preventDefault();
 		try {
 			const user = await signInWithEmailAndPassword(
 				auth,
@@ -27,35 +41,43 @@ function login() {
 				loginPassword
 			);
 			console.log(user);
-			window.sessionStorage.setItem("email", user.user.email);
 		} catch (error) {
 			console.error("Create User error...", error.message);
 		}
+		props.history.push("/");
 	};
 
 	return (
 		<div className="login-page">
 			<div className="login-wrapper">
 				<h1> Login User </h1>
-				<input
-					placeholder="Email..."
-					onChange={(event) => {
-						setloginEmail(event.target.value);
-					}}
-				/>
-				<input
-					placeholder="Password..."
-					onChange={(event) => {
-						setloginPassword(event.target.value);
-					}}
-				/>
+				<form onSubmit={login}>
+					<input
+						placeholder="Email..."
+						type="email"
+						onChange={(event) => {
+							setloginEmail(event.target.value);
+						}}
+					/>
+					<input
+						placeholder="Password..."
+						type="password"
+						onChange={(event) => {
+							setloginPassword(event.target.value);
+						}}
+					/>
 
-				<div className="bottom-buttons-wrapper">
-					<NavLink to="/create-account" className="login-link">
-						Create Account
-					</NavLink>
-					<button onClick={login}> Login </button>
-				</div>
+					<div className="bottom-buttons-wrapper">
+						<NavLink to="/create-account" className="login-link">
+							Create Account
+						</NavLink>
+						<input
+							type="submit"
+							value="Login"
+							className="login-button"
+						/>
+					</div>
+				</form>
 			</div>
 		</div>
 	);
